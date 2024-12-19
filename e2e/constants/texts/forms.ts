8500db940eb19edbe9c354b9973d574013753342
devanps212 <devanps212@gmail.com>
2024-12-19 21:14:54 +0530

    added constants for form check

--- e2e/constants/texts/forms.ts
+++ e2e/constants/texts/forms.ts
@@ -2,3 +2,9 @@ export const FORM_LABELS = {
     fullName: 'Full name',
     phNO:'Phone number'
 }
+
+export const FORM_INPUT_CHECK = {
+    emailBlock: "Question 1Email address*",
+    fulnameBlock: "Question 1Full name*",
+    phoneNumberBlock: "Question 1Phone number*🇺🇸+" 
+}
