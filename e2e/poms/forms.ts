139fee8be775b403e4b09f40d2464c14498a1e55
devanps212 <devanps212@gmail.com>
2024-12-19 19:58:55 +0530

    input added function dynamically

--- e2e/poms/forms.ts
+++ e2e/poms/forms.ts
@@ -1,11 +1,23 @@
-import { Page } from "@playwright/test";
+import { expect, Page } from "@playwright/test";
 
 export default class UserForm{
     constructor(
         private page: Page
     ){}
 
-    createAndSubmitForm = async()=>{
-        
+    formInputCreation = async({
+        labels
+    }:{
+        labels: string[]
+    })=>{
+        let count = 1
+        for(const label of labels){
+            const button = this.page.getByRole('button', { name: label })
+            await button.scrollIntoViewIfNeeded()
+            await button.click()
+            await expect(this.page.getByRole('button', { name: 'Question' }).nth(count)).toBeVisible()
+            count++
+        }
+
     }
 }
