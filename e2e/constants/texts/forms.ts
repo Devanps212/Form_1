1648d8b4c06a693a4811c8124751513879d8c75b
devanps212 <devanps212@gmail.com>
2024-12-20 00:33:01 +0530

    added constants for default value input addding

--- e2e/constants/texts/forms.ts
+++ e2e/constants/texts/forms.ts
@@ -8,3 +8,10 @@ export const FORM_INPUT_CHECK = {
     fullnameBlock: "Question 1Full name*",
     phoneNumberBlock: "Question 1Phone number*🇺🇸+" 
 }
+
+export const SUBMISSION_USER_DETAILS = {
+    email:"sam@example.com",
+    firstName:"sam",
+    lastName:"Alex",
+    phNo:"212 555 1212"
+}
