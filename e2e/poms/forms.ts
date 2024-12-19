498313a06382ebaff0cbe6dd4b465687ac7cc412
devanps212 <devanps212@gmail.com>
2024-12-19 18:46:59 +0530

    POM for form tasks added

---
+++ e2e/poms/forms.ts
@@ -0,0 +1,11 @@
+import { Page } from "@playwright/test";
+
+export default class UserForm{
+    constructor(
+        private page: Page
+    ){}
+
+    createAndSubmitForm = async()=>{
+        
+    }
+}
