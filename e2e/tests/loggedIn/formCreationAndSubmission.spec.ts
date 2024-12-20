6cec1582aca337047ed1885f9e6fbc0067b455c4
devanps212 <devanps212@gmail.com>
2024-12-20 19:00:31 +0530

    code refactorng done

--- e2e/tests/loggedIn/formCreationAndSubmission.spec.ts
+++ e2e/tests/loggedIn/formCreationAndSubmission.spec.ts
@@ -1,13 +1,14 @@
 import { expect, Locator, Page } from "@playwright/test";
-import { FORM_INPUT_CHECK, FORM_LABELS, SUBMISSION_USER_DETAILS } from '../../constants/texts/index';
+import { FORM_INPUT_CHECK, FORM_LABELS, FORM_MESSAGES, SUBMISSION_USER_DETAILS } from '../../constants/texts/index';
+import { FORM_PUBLISH_SELECTORS, INSIGHT_SELECTORS, FORM_INPUT_SELECTORS } from "../../constants/selectors";
 import { test } from "../../fixtures";
 import UserForm from "../../poms/forms";
 
 test.describe("Form page", ()=>{
     test.beforeEach("should goto form creation page", async({page}:{page: Page})=>{
         await page.goto('/admin/dashboard/active')
-        await page.getByRole('button', { name: 'Add new form' }).click()
-        await page.getByText('Start from scratchA blank').click()
+        await page.getByRole('button', { name: FORM_LABELS.addNewForm }).click()
+        await page.getByText(FORM_LABELS.startFromScratch).click()
     })
 
     test("should create and submit the form", async({
@@ -26,13 +27,13 @@ test.describe("Form page", ()=>{
             ]
             await form.formInputCreation({labels})
             await page.getByRole('button', { name: 'Submit' }).click()
-            await page.getByTestId('publish-button').click()
+            await page.getByTestId(FORM_PUBLISH_SELECTORS.publishButton).click()
             await expect(page.getByText('The form is successfully')).toBeVisible()
         })
 
         await test.step("Step 3: Verify field visibility and handle errors", async () => {
             const page1Promise = page.waitForEvent('popup')
-            await page.getByTestId('publish-preview-button').click()
+            await page.getByTestId(FORM_PUBLISH_SELECTORS.publishPreviewButton).click()
         
             const inputBlocks = [
                 FORM_INPUT_CHECK.fullnameBlock,
@@ -42,9 +43,9 @@ test.describe("Form page", ()=>{
             await form.forInputCheck({ inputBlocks })
             const page1 = await page1Promise
         
-            const email = page1.locator('input[data-cy="email-text-field"]')
+            const email = page1.locator(FORM_INPUT_SELECTORS.email)
             await email.fill('hello')
-            const phone = page1.locator('input[type="tel"]')
+            const phone = page1.locator(FORM_INPUT_SELECTORS.inputTel)
             await phone.fill("dcd")
         
             const submit = page1.getByRole('button', { name: 'Submit' })
@@ -56,11 +57,11 @@ test.describe("Form page", ()=>{
 
             await email.fill(SUBMISSION_USER_DETAILS.email)
             await phone.fill(SUBMISSION_USER_DETAILS.phNo)
-            await page1.locator('input[data-cy="first-name-text-field"]').fill(SUBMISSION_USER_DETAILS.firstName)
-            await page1.locator('input[data-cy="last-name-text-field"]').fill(SUBMISSION_USER_DETAILS.lastName)
+            await page1.locator(FORM_INPUT_SELECTORS.firstName).fill(SUBMISSION_USER_DETAILS.firstName)
+            await page1.locator(FORM_INPUT_SELECTORS.lastName).fill(SUBMISSION_USER_DETAILS.lastName)
 
             await submit.click()
-            await expect(page1.locator('div').filter({ hasText: '🎉Thank You.Your response has' })
+            await expect(page1.locator('div').filter({hasText: FORM_MESSAGES.formSubmitSuccess})
             .nth(3)).toBeVisible({timeout:60000})
             await page1.close()
         })
@@ -87,10 +88,9 @@ test.describe("Form page", ()=>{
         await test.step("Step 2:Create and hide multi choice", async()=>
             await form.multiChoiceAndSingleChoice({label:"multi"}))
 
-
         await test.step("Step 3:Publish form", async()=>{
-            await page.getByTestId('publish-button').click({timeout:30000})
-            await expect(page.getByText('The form is successfully')).toBeVisible({timeout:30000})
+            await page.getByTestId(FORM_PUBLISH_SELECTORS.publishButton).click({timeout:30000})
+            await expect(page.getByText(FORM_MESSAGES.formCreationSuccess)).toBeVisible({timeout:30000})
         })
 
         await test.step("Step 4:Check the options are randmized", async()=>{
@@ -98,16 +98,16 @@ test.describe("Form page", ()=>{
             const NormalOrder = Array.from({ length: 10 }, (_, i) => `Option ${i + 1}`);
 
             const page1Promise = page.waitForEvent('popup')
-            await page.getByTestId('publish-preview-button').click()
+            await page.getByTestId(FORM_PUBLISH_SELECTORS.publishPreviewButton).click()
   
             const page1 = await page1Promise
-            const fieldset = page.locator('[data-cy="multiple-choice-group"]')
+            const fieldset = page.locator(FORM_INPUT_SELECTORS.multiChoice)
             await expect(fieldset).toBeHidden()
-            const randomizedOrder = await page1.locator(".neeto-form-single-choice__item").allTextContents()
+            const randomizedOrder = await page1.locator(FORM_INPUT_SELECTORS.singleChoice)
+            .allTextContents()
             expect(randomizedOrder).not.toEqual(NormalOrder)
             await page1.close()
         })
-
 
         await test.step("Step 5:Unhide multiChoice", async()=>{
             const multiChoice = page.getByRole('button', { name: 'Question' }).nth(2)
@@ -118,13 +118,13 @@ test.describe("Form page", ()=>{
             .locator('label').nth(1)
             .click({timeout:40000})
 
-            await page.getByTestId('publish-button').click()
-            await expect(page.getByText('The form is successfully')).toBeVisible({timeout:30000})
+            await page.getByTestId(FORM_PUBLISH_SELECTORS.publishButton).click()
+            await expect(page.getByText(FORM_MESSAGES.formCreationSuccess)).toBeVisible({timeout:30000})
         })
 
         await test.step("Step 6:Check the visiibilty of multiChoice", async()=>{
             const page1Promise = page.waitForEvent('popup', {timeout:30000})
-            await page.getByTestId('publish-preview-button').click()
+            await page.getByTestId(FORM_PUBLISH_SELECTORS.publishPreviewButton).click()
   
             const page1 = await page1Promise
             const fieldset = page.getByText('Question 1multi Demo field*')
@@ -145,27 +145,31 @@ test.describe("Form page", ()=>{
         let submissions = 0
         let completion = 0
 
-
         let visitsMetric :Locator;
         let startsMetric :Locator;
         let submissionsMetric :Locator;
         let completionMetric :Locator;
 
         await test.step("Step 1:Check insights to enuse everything is 0", async()=>{
-            await page.getByTestId('publish-button').click()
+            await page.getByTestId(FORM_PUBLISH_SELECTORS.publishButton).click()
             await page.getByRole('link', { name: 'Submissions' }).click()
             await page.getByRole('link', { name: 'Insights' }).click()
 
-            visitsMetric = page.locator('div').filter({ hasText: new RegExp(`^${visitCount}Visits$`) }).getByTestId('insights-count')
-            startsMetric = page.locator('div').filter({ hasText: new RegExp(`^${starts}Starts$`) }).getByTestId('insights-count')
-            submissionsMetric = page.locator('div').filter({ hasText: new RegExp(`^${submissions}Submissions$`) }).getByTestId('insights-count')
+            visitsMetric = page.locator('div')
+                .filter({ hasText: new RegExp(`^${visitCount}Visits$`) })
+                .getByTestId(INSIGHT_SELECTORS.insightCount)
+            startsMetric = page.locator('div')
+                .filter({ hasText: new RegExp(`^${starts}Starts$`) })
+                .getByTestId(INSIGHT_SELECTORS.insightCount)
+            submissionsMetric = page.locator('div')
+                .filter({ hasText: new RegExp(`^${submissions}Submissions$`) })
+                .getByTestId(INSIGHT_SELECTORS.insightCount)
             completionMetric = page.getByRole('heading', { name: `${completion}%` })
 
             await expect(visitsMetric).toHaveText(`${visitCount}`)
             await expect(startsMetric).toHaveText(`${starts}`)
             await expect(submissionsMetric).toHaveText(`${submissions}`)
             await expect(completionMetric).toHaveText(`${completion}%`)
-
         })
         
         
